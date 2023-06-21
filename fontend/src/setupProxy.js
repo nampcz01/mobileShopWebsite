f3363e219c7ec314b9dc3806c7f62fa560b7c54a
TranVu2582410 <trananhvu2016kg@gmail.com>
2023-06-21 13:47:11 +0700

    uninstall http_proxy_midlleware

--- fontend/src/setupProxy.js
+++
@@ -1,8 +0,0 @@
-import { createProxyMiddleware } from "http-proxy-middleware";
-
-
-export default app =>{
-    app.use(
-        createProxyMiddleware('/api/products',{target: 'http://localhost:9000/api/products'})
-    )
-}
