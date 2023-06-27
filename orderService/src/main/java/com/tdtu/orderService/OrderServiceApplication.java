2ea021f4d235b2e66f64d968950f2fab659067cc
Hoang Nam <nampcz001@gmail.com>
2023-06-27 07:49:23 +0700

    MOB-20 : [Back-end] implemt order service

---
+++ orderService/src/main/java/com/tdtu/orderService/OrderServiceApplication.java
@@ -0,0 +1,28 @@
+package com.tdtu.orderService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceApplication.class, args);
+	}
+	
+	@Bean
+	public WebMvcConfigurer configure() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry reg) {
+				reg.addMapping("/**").allowedOrigins("*");
+			}
+		};
+	}
+
+}
