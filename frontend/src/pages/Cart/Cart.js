29562a05f8708fee9a0c83e4063a034be2c3ba7c
TranVu2582410 <trananhvu2016kg@gmail.com>
2023-06-29 04:10:19 +0700

    add cart-page-empty

--- frontend/src/pages/Cart/Cart.js
+++ frontend/src/pages/Cart/Cart.js
@@ -1,7 +1,18 @@
+import classNames from "classnames/bind";
+import styles from "./Cart.module.scss";
+const cx = classNames.bind(styles)
+
 function Cart() {
     return (
-    <div>
-        <h2>Cart page</h2>
+    <div className={cx('wrapper')}>
+        <div className={cx('item')}>
+            <img height={'400px'} src="https://www.october16th.store/assets/empty-cart-e2d16cf8.jpg" alt="" />
+            <div className={cx('text')}>
+                <span>Bạn chưa có sản phẩm nào trong giỏ hàng</span>
+                <span>Hãy mua gì đó!</span>
+            </div>
+            <a href="/"><button className={cx('btnHome')}>Tiếp tục mua sắm</button></a>
+        </div>
     </div>
     )
 }
