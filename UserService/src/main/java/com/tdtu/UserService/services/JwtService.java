6bf176de86a24e2c0b91fbf38935193acc263e60
Hoang Nam <nampcz001@gmail.com>
2023-07-09 09:53:15 +0700

    MOB-17 [Back-end] Implement spring sercurity in the project

---
+++ UserService/src/main/java/com/tdtu/UserService/services/JwtService.java
@@ -0,0 +1,68 @@
+package com.tdtu.UserService.services;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Component
+public class JwtService {
+
+
+    public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
+
+
+    @Value("${secret}")
+    private String jwtSecret;
+
+    private Key getSigningKey() {
+        String encodedKey = Base64.getEncoder().encodeToString(this.jwtSecret.getBytes(StandardCharsets.UTF_8));
+        byte[] keyBytes = Decoders.BASE64.decode(encodedKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+    public Boolean validateJwtToken(String token, UserDetails userDetails) {
+        String username = getUsernameFromToken(token);
+        Claims claims = Jwts.parser().setSigningKey(getSigningKey()).parseClaimsJws(token).getBody();
+        Boolean isTokenExpired = claims.getExpiration().before(new Date());
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired);
+    }
+    
+    public String getUsernameFromToken(String token) {
+        final Claims claims = Jwts.parser().setSigningKey(getSigningKey()).parseClaimsJws(token).getBody();
+        return claims.getSubject();
+    }
+
+
+    public String generateToken(String userName) {
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims, userName);
+    }
+
+    private String createToken(Map<String, Object> claims, String userName) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(userName)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30))
+                .signWith(SignatureAlgorithm.HS256, getSignKey()).compact();
+    }
+
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
---
+++ apiGateway/src/main/java/com/tdtu/apiGateway/filter/RouteValidator.java
@@ -0,0 +1,24 @@
+package com.tdtu.apiGateway.filter;
+
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+
+import java.util.*;
+import java.util.function.Predicate;
+
+@Component
+public class RouteValidator {
+
+    public static final List<String> openApiEndpoints = List.of(
+            "/api/users/register",
+            "/api/users/login",
+            "/api/products",
+            "/api/products/getDetail/**"
+    );
+
+    public Predicate<ServerHttpRequest> isSecured =
+            request -> openApiEndpoints
+                    .stream()
+                    .noneMatch(uri -> request.getURI().getPath().contains(uri));
+
+}
